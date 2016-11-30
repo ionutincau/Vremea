862ae9ac2cc9918ad27c3bb94d5c35e81606f8f9
ionutincau <ionut.incau@gmail.com>
2016-11-30 20:38:24 +0200

    Show city id on map

--- app/src/main/java/com/example/ionut/vremea2/MainActivity.java
+++ app/src/main/java/com/example/ionut/vremea2/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.ionut.vremea2;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -30,6 +34,21 @@ public class MainActivity extends Activity {
         }
         if (id == R.id.action_settings) {
             this.startActivity(new Intent(this, SettingsActivity.class));
+        }
+        if (id == R.id.action_map) {
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+            String location = preferences.getString(getString(R.string.pref_cities_key), getString(R.string.pref_cities_default));
+            Uri geoLocation = Uri.parse("geo:0,0?").buildUpon().appendQueryParameter("q", location).build();
+
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(geoLocation);
+
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            }
+            else {
+                Log.d(this.getClass().getSimpleName(), "Couldn't call " + location + ", no receiving apps installed!");
+            }
         }
         return true;
     }
